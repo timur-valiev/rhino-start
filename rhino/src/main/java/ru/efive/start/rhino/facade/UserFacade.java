a141574f5faedef1688cd84c7838aaf514e10ed7
Timur Valiev <zloi.timur@gmail.com>
2012-04-10 13:08:30 +0700

    context fixed

--- rhino/src/main/java/ru/efive/start/rhino/facade/UserFacade.java
+++ rhino/src/main/java/ru/efive/start/rhino/facade/UserFacade.java
@@ -6,6 +6,8 @@ import org.springframework.transaction.annotation.Transactional;
 import ru.efive.start.rhino.domain.User;
 import ru.efive.start.rhino.persistence.UserRepository;
 
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
 @Service
@@ -29,4 +31,18 @@ public class UserFacade {
     public void addUser(User user) {
         userRepository.addUser(user);
     }
+
+    @Transactional
+    public void addUser(String email, String pass) {
+        try {
+            userRepository.addUser(new User(email, pass));
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
+
 }
